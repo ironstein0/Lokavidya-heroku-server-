6ec5104d9041615e3e7118431e959ca457d91baa
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-02-14 13:57:25 +0530

    test

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ try {
 
 // Heroku dynamically assigns your app a port, so you can't set the port 
 // to a fixed number. Heroku adds the port to the env, so you can pull it from there.
-var port = process.env.PORT || 5000;
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-var cloudUri = process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js';
-var appId = process.env.APP_ID || config.APP_ID;
-var masterKey = process.env.masterKey || config.MASTER_KEY;
-var serverUrl = process.env.SERVER_URL || 'http://localhost:' + port + '/parse';
+const PORT = process.env.PORT || 5000;
+const DATABASE_URI = process.env.DATABASE_URI || process.env.MONGODB_URI;
+const CLOUD_URL = process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js';
+const APP_ID = process.env.APP_ID || config.APP_ID;
+const MASTER_KEY = process.env.masterKey || config.MASTER_KEY;
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + port + '/parse';
 
 if (!databaseUri) {
   throw new Error('DATABASE_URI not specified');
@@ -29,11 +29,11 @@ if (!databaseUri) {
 
 // generate parse-server middleware
 var api = new ParseServer({
-	databaseURI: databaseUri,
-	cloud: cloudUri
-	appId: appId,
-	masterKey: masterKey,
-	serverURL: serverUrl,
+	databaseURI: DATABASE_URI,
+	cloud: CLOUD_URL,
+	appId: APP_ID,
+	masterKey: MASTER_KEY,
+	serverURL: SERVER_URL,
 	liveQuery: {
 		classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
 	}
@@ -45,7 +45,7 @@ Parse.initialize('app', 'master');
 
 // instantiate express
 var app = express();
-app.set('port', port);
+app.set('port', PORT);
 
 // default route
 app.get('/', (req, res) => {
@@ -57,6 +57,6 @@ app.get('/', (req, res) => {
 app.use('/parse', api);
 
 // start http server
-app.listen(port, function() {
-  console.log('Node app is running on port' + port);
+app.listen(PORT, function() {
+  console.log('Node app is running on port' + PORT);
 });
